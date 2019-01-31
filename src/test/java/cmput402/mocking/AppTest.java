1464218ffecf75fea91048728db5e335137602c2
Blaz Pocrnja <blaz@cs.ualberta.ca>
2019-01-30 18:59:16 -0700

    Added mockito

--- src/test/java/cmput402/mocking/AppTest.java
+++ src/test/java/cmput402/mocking/AppTest.java
@@ -1,8 +1,17 @@
 package cmput402.mocking;
 
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
+import service.City;
 import stub.CityStub;
 import stub.SalaryStub;
 
@@ -38,6 +47,29 @@ public class AppTest
     	Util util = new Util();
     	CityStub stub = new CityStub();
     	assert(3 == util.filterEdmonton(stub));
+    }
+    
+    public void testFilterEdmontonMockito() {
+    	Util util = new Util();
+    	City mockCity = mock(City.class);
+    	
+    	List<String> listCities = new ArrayList<String>();
+    	listCities.add("Edmonton");
+    	listCities.add("Calgary");
+    	listCities.add("Edmonton");
+    	listCities.add("Calgary");
+    	
+    	List<String> listCities2 = new ArrayList<String>();
+    	listCities2.add("Edmonton");
+    	listCities2.add("Calgary");
+    	listCities2.add("Calgary");
+    	
+    	when(mockCity.listCities()).thenReturn(listCities).thenReturn(listCities2);
+    	
+    	assert(2 == util.filterEdmonton(mockCity));
+    	assert(1 == util.filterEdmonton(mockCity));
+    	
+    	verify(mockCity, times(2)).listCities();
     }
     
     public void testFilterSalaries(){
